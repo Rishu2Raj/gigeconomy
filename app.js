6138438cdb5849500fb9a41c9cbb271142b362b6
RISHU RAJ <rishurajofficial06@gmail.com>
2025-03-24 15:23:20 +0530

    show route and toLocaleString

--- app.js
+++ app.js
@@ -19,11 +19,19 @@ async function main(){
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
 
 //index route
 app.get('/listings', async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
+})
+
+//show route
+app.get('/listings/:id', async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
 })
 
 app.listen(8080, () => {
