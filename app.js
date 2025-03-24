4f150d98c71ad7b7120e4ba32859cb39933274df
RISHU RAJ <rishurajofficial06@gmail.com>
2025-03-24 22:06:13 +0530

    delete route and some correction in update route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const Listing = require("./models/listing.js")
 const path = require('path');
-var methodOverride = require('method-override');
+const methodOverride = require('method-override');
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/gigeconomy";
 
@@ -21,7 +21,7 @@ async function main(){
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 
 //index route
@@ -57,10 +57,18 @@ app.get('/listings/:id/edit', async (req, res) => {
 })
 
 //update route
-app.post('/listings/:id', async (req, res) => {
+app.put('/listings/:id', async (req, res) => {
     let {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
+})
+
+//delete route
+app.delete('/listings/:id', async (req, res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
 })
 
 app.listen(8080, () => {
