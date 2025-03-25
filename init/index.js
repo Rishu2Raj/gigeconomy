78b1e8b06b1ecc5cb3eff3a63d003e7ef904ebb7
RISHU RAJ <rishurajofficial06@gmail.com>
2025-03-25 12:27:46 +0530

    edit and new page styling

--- init/index.js
+++ init/index.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const initData = require('./data.js');
 const Listing = require('../models/listing.js');
-const { log } = require('console');
-
 const MONGO_URL = "mongodb://127.0.0.1:27017/gigeconomy";
 
 main()
