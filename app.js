b159717ed214aae1d1af5ddf7b0be5daf6da2ff3
RISHU RAJ <rishurajofficial06@gmail.com>
2025-03-27 19:32:07 +0530

    adding wrapAsync

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing.js")
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const wrapAsync = require("./utils/wrapAsync.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/gigeconomy";
 
@@ -45,16 +46,12 @@ app.get('/listings/:id', async (req, res) => {
 })
 
 //create route
-app.post('/listings', async (req, res, next) => {
-    try {
+app.post('/listings', wrapAsync( async (req, res, next) => {
         // let {title, description, image, price, location, country} = req.body;
         const newListing = new Listing(req.body.listing);
         await newListing.save();
-        res.redirect("/listings");
-    } catch (err) {
-        next(err);
-    }
-})
+        res.redirect("/listings");    
+}));
 
 //edit route
 app.get('/listings/:id/edit', async (req, res) => {
