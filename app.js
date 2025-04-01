9312e0b512f0737ba3aa5844620775f4db0bfdaf
RISHU RAJ <rishurajofficial06@gmail.com>
2025-04-02 02:04:01 +0530

    express-session

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const ejsMate = require('ejs-mate');
 const ExpressError = require("./utils/ExpressError.js");
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
+const session = require('express-session');
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/gigeconomy";
 
@@ -21,12 +22,27 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+const sessionOption = {
+    secret: 'mysupersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
+  };
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+
+app.use(session(sessionOption));
 
 
 app.use("/listings", listings);
