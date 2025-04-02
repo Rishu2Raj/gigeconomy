90eabbe2e128462685850534218f123b51ca23df
RISHU RAJ <rishurajofficial06@gmail.com>
2025-04-02 11:04:32 +0530

    setting up passport

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/gigeconomy";
 
@@ -44,6 +47,12 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(session(sessionOption));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use(flash());
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
