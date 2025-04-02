4f6fee87f9e1d3a68fa1151209e453562cc1db8e
RISHU RAJ <rishurajofficial06@gmail.com>
2025-04-02 12:19:24 +0530

    functional /login route

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const userRouter = require("./routes/user.js")
 const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require("passport");
-const localStrategy = require("passport-local");
+const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/gigeconomy";
@@ -50,7 +50,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use(session(sessionOption));
 app.use(passport.initialize());
 app.use(passport.session());
-
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,11 @@ const express = require("express");
 const wrapAsync = require("../utils/wrapAsync");
 const router = express.Router();
 const User = require("../models/user.js");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
-})
+});
 
 router.post("/signup", wrapAsync( async (req, res) => {
     try{
@@ -20,5 +21,14 @@ router.post("/signup", wrapAsync( async (req, res) => {
         res.redirect("/signup");
     }
 }));
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+router.post("/login", passport.authenticate("local", { failureRedirect: '/login', failureFlash: true, }), async (req, res) => {
+    req.flash("success", "Welcome back to WanderLust!");
+    res.redirect("/listings");
+});
 
 module.exports = router;
