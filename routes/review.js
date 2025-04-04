2ac049b12ba947b3a9fde91312e527b8dd72ea69
RISHU RAJ <rishurajofficial06@gmail.com>
2025-04-04 14:12:29 +0530

    authorization for reviews

--- routes/review.js
+++ routes/review.js
@@ -3,15 +3,16 @@ const router = express.Router({mergeParams: true});
 const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const Review = require("../models/review.js");
-const {validateReview} = require("../middleware.js");
+const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
 
 
 // review
 // post route
-router.post("/", validateReview, wrapAsync(async(req, res) => {
+router.post("/", isLoggedIn, validateReview, wrapAsync(async(req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
     listing.reviews.push(newReview);
+    newReview.author = req.user._id;
     await newReview.save();
     await listing.save();
     req.flash('success', 'New Review Created!');
@@ -19,7 +20,7 @@ router.post("/", validateReview, wrapAsync(async(req, res) => {
 }));
 
 // Delete Review Route
-router.delete("/:reviewId", wrapAsync(async (req, res) => {
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(async (req, res) => {
     let {id, reviewId} = req.params;
     await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
     
