c46b80402c6137cfa3535865707e2477f3c61440
RISHU RAJ <rishurajofficial06@gmail.com>
2025-04-06 07:47:31 +0530

    map popup

--- init/index.js
+++ init/index.js
@@ -18,7 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({...obj, owner:"67ece7488cfe2def0d963926"}));
+  // initData.data = initData.data.map((obj) => ({...obj, owner:"67ece7488cfe2def0d963926"}));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
--- public/js/map.js
+++ public/js/map.js
@@ -2,10 +2,20 @@ maptilersdk.config.apiKey = mapToken;
 const map = new maptilersdk.Map({
     container: 'map', // container's id or the HTML element to render the map
     style: maptilersdk.MapStyle.STREETS,
-    center: coordinates,
+    center: listing.geometry.coordinates,
     zoom: 7,
 });
-console.log(coordinates);
+
+ // create the popup
+ var popup = new maptilersdk.Popup({ offset: 25 }).setHTML(
+    `<h4>${listing.title}</h4><p>Exact location provided after booking.</p>`
+);
+
+// create DOM element for the marker
+var el = document.createElement('div');
+el.id = 'marker';
+
 const marker = new maptilersdk.Marker({ color: "red" })
-    .setLngLat(coordinates)
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(popup)
     .addTo(map);
